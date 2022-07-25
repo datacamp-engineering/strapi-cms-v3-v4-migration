1bb89bdb984527419c3b9b9318aab24cb5cf4288
Gary Alway <garydouglasalway@gmail.com>
2022-07-25 08:32:44 +0200

    ignore redundant tables

--- v3-sql-v4-sql/migrate/helpers/migrate.js
+++ v3-sql-v4-sql/migrate/helpers/migrate.js
@@ -8,7 +8,82 @@ const {
 const { BATCH_SIZE } = require("./constants");
 const { migrateItems } = require("./migrateFields");
 
+const tables_to_ignore = [
+  "components_page_features",
+  "components_page_content_menus",
+  "components_page_items",
+  "components_page_quotes_sections",
+  "components_page_top_scroll_sections",
+  "components_page_join_companies_blocks",
+  "components_page_footer_contents",
+  "components_page_headers",
+  "components_page_section_items",
+  "components_page_bottom_scroll_sections",
+  "components_page_header_content_blogs",
+  "components_page_quotes_slider_items",
+  "components_page_help_tabs_items",
+  "components_page_sections",
+  "components_page_image_tips",
+  "components_page_links",
+  "components_page_schedule_call_form_landing_pages",
+  "components_page_content_footers",
+  "components_page_posted_ins",
+  "components_page_quotes_sliders",
+  "components_page_scroll_items",
+  "components_page_get_starters",
+  "components_page_company_highlight",
+  "components_blog_podcast_headers",
+  "components_page_help_tabs",
+  "components_page_resources_list_items",
+  "components_seo_seos",
+  "components_page_company_images",
+  "components_page_resources_lists",
+  "components_page_rounded_page_titles",
+  "components_page_resources_carousels",
+  "components_page_image_text_blocks",
+  "components_page_notorious_companies",
+  "components_page_resources",
+  "components_page_interactive_hero_image_landing_pages",
+  "components_page_bottom_scroll_sections_components",
+  "components_page_footer_contents_components",
+  "components_page_quotes_sections_components",
+  "components_page_scroll_items_components",
+  "components_page_top_scroll_sections_components",
+  "components_page_company_highlight_components",
+  "components_page_header_content_blogs_components",
+  "components_page_content_footers_components",
+  "components_page_notorious_companies_components",
+  "components_page_interactive_hero_image_landing_pages_components",
+  "components_page_resources_carousels_components",
+  "components_page_help_tabs_components",
+  "components_page_quotes_sliders_components",
+  "components_page_schedule_call_form_landing_pages_components",
+  "components_page_sections_components",
+  "navs_components",
+  "articles",
+  "articles_components",
+  "pages",
+  "pages_components",
+  "technologies",
+  "navs",
+  "top_viewed_blogs",
+  "topics",
+  "local_health",
+  "components_page_trendings",
+  "category_index_pages",
+  "category_index_pages_components",
+  "components_page_columnar_page_headers",
+  "components_page_three_column_info_blocks",
+  "components_page_headers_components",
+  "components_page_resources_lists_components",
+];
+
 async function migrate(source, destination, itemMapper = undefined) {
+  if (tables_to_ignore.includes(source)) {
+    console.log(`skipping migration for ${source}`);
+    return false;
+  }
+
   if (isMYSQL) {
     const sourceNotExists =
       (await dbV3.raw(`SHOW TABLES LIKE '%${source}%';`))[0].length === 0;
@@ -121,7 +196,9 @@ async function migrate(source, destination, itemMapper = undefined) {
 
     const migratedItems = migrateItems(withParsedJsonFields, itemMapper);
     if (migratedItems.length > 0) {
-      await dbV4(destination).insert(migratedItems);
+      await dbV4(destination)
+        .insert(migratedItems)
+        .catch((err) => console.log(err));
     }
   }
 
