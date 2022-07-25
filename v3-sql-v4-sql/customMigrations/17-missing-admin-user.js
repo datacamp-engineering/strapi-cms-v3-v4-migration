f9b124f039b259c6134e39b6113be035f4cd204f
Gary Alway <garydouglasalway@gmail.com>
2022-07-25 14:12:15 +0200

    add dummy user to prevent a chain of fk violation errors

---
+++ v3-sql-v4-sql/customMigrations/17-missing-admin-user.js
@@ -0,0 +1,14 @@
+const { dbV4 } = require("../config/database");
+
+const processedTables = [];
+
+async function migrateTables() {
+  await dbV4.raw(
+    "insert into admin_users (id, username, blocked, email) values (8, 'migration-fix', true, 'migration.fix@datacamp.com');"
+  );
+}
+
+module.exports = {
+  processedTables,
+  migrateTables,
+};
